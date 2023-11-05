be39d50d740c70a8415be8171404189f4ef53caf
unknown <hoangducbmt1997@gmail.com>
2023-11-05 15:05:34 +0700

    reafator code

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
-const express = require('express');
-const morgan = require('morgan');
-const handlebars  = require('express-handlebars');
-const path = require('path');
-
-const port = 3000
-
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const port = 3000;
 const app = express();
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+// import file route
+const route = require("./routes");
 
+// Template
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
 
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
-// Create Route
-app.get('/', (req, res) => {
-  res.render('home');
-});
+// route init
+route(app);
 
-app.get('/news', (req, res) => {
-    res.render('news');
-  });
-
-// Khởi động máy chủ
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
